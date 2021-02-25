00520ad46c4e602c62b57aed60d05576fd1a09b8
HARDIK KANZARIYA <kanzariyahardikkumar@gmail.com>
2021-02-25 18:09:47 +0530

    search done

--- app/src/main/java/com/tcc/app/activity/SearchActivity.kt
+++ app/src/main/java/com/tcc/app/activity/SearchActivity.kt
@@ -2,17 +2,40 @@ package com.tcc.app.activity
 
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.tcc.app.R
 import com.tcc.app.extention.getValue
+import com.tcc.app.extention.showAlert
 import com.tcc.app.extention.visible
 import com.tcc.app.fragment.CustomerFragment
+import com.tcc.app.fragment.EmployeeFragment
 import com.tcc.app.fragment.LeadFragment
+import com.tcc.app.modal.UserTypeDataItem
+import com.tcc.app.modal.UserTypeListModel
+import com.tcc.app.network.CallbackObserver
+import com.tcc.app.network.Networking
+import com.tcc.app.network.addTo
 import com.tcc.app.utils.Constant
+import com.tcc.app.utils.Logger
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_search.*
 import kotlinx.android.synthetic.main.toolbar_with_back_arrow.*
+import org.json.JSONException
+import org.json.JSONObject
+import tech.hibk.searchablespinnerlibrary.SearchableDialog
+import tech.hibk.searchablespinnerlibrary.SearchableItem
 
 class SearchActivity : BaseActivity() {
     var type: String? = ""
+
+    var userTypeNameList: ArrayList<String>? = null
+    var adapterUserType: ArrayAdapter<String>? = null
+    var userTypeListArray: ArrayList<UserTypeDataItem>? = null
+    var itemUserType: List<SearchableItem>? = null
+    var usertypeId: String = "-1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +48,12 @@ class SearchActivity : BaseActivity() {
 
         txtTitle.text = getString(R.string.filter)
 
+        userTypeNameList = ArrayList()
+        userTypeListArray = ArrayList()
+
         getBundleData()
+
+
 
         btnSubmit.setOnClickListener {
             filter()
@@ -37,6 +65,17 @@ class SearchActivity : BaseActivity() {
         if (type.equals(Constant.LEAD) || type.equals(Constant.CUSTOMER)) {
             inEmail.visible()
             inName.visible()
+        }
+        if (type.equals(Constant.EMPLOYEE)) {
+            inEmail.visible()
+            inName.visible()
+            linlayUserType.visible()
+
+            getUserTypeList()
+
+            userTypeViewClick()
+
+            userTypeSpinnerListner()
         }
     }
 
@@ -51,6 +90,97 @@ class SearchActivity : BaseActivity() {
             CustomerFragment.name = edtName.getValue()
             finish()
         }
+        if (type.equals(Constant.EMPLOYEE)) {
+            EmployeeFragment.email = edtEmail.getValue()
+            EmployeeFragment.name = edtName.getValue()
+            EmployeeFragment.usertypeid = usertypeId
+            finish()
+        }
+    }
 
+    private fun userTypeViewClick() {
+
+        view2.setOnClickListener {
+            SearchableDialog(this@SearchActivity,
+                itemUserType!!,
+                getString(R.string.select_usertype), { item, _ ->
+                    spUserType.setSelection(item.id.toInt())
+                }).show()
+        }
+
+    }
+
+    private fun userTypeSpinnerListner() {
+        spUserType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position != -1 && userTypeListArray!!.size > position) {
+                    if (position == 0) {
+                        usertypeId = "-1"
+                    } else {
+                        usertypeId = userTypeListArray!!.get(position - 1).usertypeID.toString()
+                    }
+                    Logger.d("userIDq", usertypeId)
+
+                }
+
+            }
+        }
+    }
+
+    fun getUserTypeList() {
+        var result = ""
+        try {
+            val jsonBody = JSONObject()
+            jsonBody.put("StateID", "")
+
+            result = Networking.setParentJsonData(Constant.METHOD_USERTYPE_LIST, jsonBody)
+
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        Networking
+            .with(this@SearchActivity)
+            .getServices()
+            .getUserTypeList(Networking.wrapParams(result))
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(object : CallbackObserver<UserTypeListModel>() {
+                override fun onSuccess(response: UserTypeListModel) {
+                    userTypeListArray!!.addAll(response.data)
+
+                    var myList: MutableList<SearchableItem> = mutableListOf()
+                    userTypeNameList!!.add("Select User Type")
+                    for (items in response.data.indices) {
+                        userTypeNameList!!.add(response.data.get(items).usertype.toString())
+                        myList.add(SearchableItem(items.toLong(), userTypeNameList!!.get(items)))
+                    }
+                    itemUserType = myList
+
+                    adapterUserType = ArrayAdapter(
+                        this@SearchActivity,
+                        R.layout.custom_spinner_item,
+                        userTypeNameList!!
+                    )
+                    spUserType.setAdapter(adapterUserType)
+
+
+                }
+
+                override fun onFailed(code: Int, message: String) {
+
+                    showAlert(message)
+
+                }
+
+            }).addTo(autoDisposable)
     }
 }
